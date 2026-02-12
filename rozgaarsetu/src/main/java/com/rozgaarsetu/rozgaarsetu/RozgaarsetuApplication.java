12135e21f9ab67908babe442c8a891ed7f83405a
ujjwal rai <ujjwalrai0429@gmail.com>
2026-02-12 22:32:55 +0530

    Day 1: Project setup, Mysql config, Swagger and health endpoint

---
+++ rozgaarsetu/src/main/java/com/rozgaarsetu/rozgaarsetu/RozgaarsetuApplication.java
@@ -0,0 +1,13 @@
+package com.rozgaarsetu.rozgaarsetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozgaarsetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RozgaarsetuApplication.class, args);
+	}
+
+}
---
+++ rozgaarsetu/src/test/java/com/rozgaarsetu/rozgaarsetu/RozgaarsetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozgaarsetu.rozgaarsetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozgaarsetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
